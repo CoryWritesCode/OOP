f1942dfeb8b0ef03f7de6ca735e242931e0565b1
CoryWritesCode <corycunningham2010@gmail.com>
2018-10-01 06:15:00 -0500

    small beginings

---
+++ Shape_up_or_shape_out/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  class Shape {
+
+    constructor(name, area, parimeter) {
+
+      this.name = name;
+      this.area = area;
+      this.parimeter =parimeter;
+
+    }
+
+  }
+
+});
