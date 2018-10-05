2949abf749a4115d47beaedeff8746f51d3e94aa
CoryWritesCode <corycunningham2010@gmail.com>
2018-10-05 06:32:42 -0500

    classes and html stuff

--- Shape_up_or_shape_out/scripts.js
+++ Shape_up_or_shape_out/scripts.js
@@ -12,4 +12,60 @@ $(document).ready(function () {
 
   }
 
+  class Circle extends Shape {
+
+    constructor(radius) {
+
+      super(name, area, parimeter)
+      this.radius = radius;
+
+    }
+
+  }
+
+  class Rectangle extends Shape {
+
+    constructor(width, height) {
+
+      super(name, area, parimeter)
+      this.width = width;
+      this.height = height;
+
+    }
+
+  }
+
+  class Square extends Shape {
+
+    constructor(sideLength) {
+
+      super(name, area, parimeter)
+      this.sideLength = sideLength;
+
+    }
+
+  }
+
+  class Circle extends Shape {
+
+    constructor(radius) {
+
+      super(name, area, parimeter)
+      this.radius = radius;
+
+    }
+
+  }
+
+  class Triangle extends Shape {
+
+    constructor(height) {
+
+      super(name, area, parimeter)
+      this.height = height;
+
+    }
+
+  }
+
 });
